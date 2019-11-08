83ff2a68eaed69d8b6430e9ce13f4782e7690bf7
justinmatsnev <jmatsnev@gmail.com>
2019-11-08 18:51:01 -0500

    first commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Justin Matsnev on 11/7/19.
+//  Copyright © 2019 Justin Matsnev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var subTotalLbl : UILabel!
+    @IBOutlet var taxLbl : UILabel!
+    @IBOutlet var tipControl : UISegmentedControl!
+    @IBOutlet var totalAmtLbl : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
